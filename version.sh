b1d102598fb8e16067108250aeb8e9faf22f131c
Shawn Toffel <shawn.toffel@gmail.com>
2019-06-06 00:47:42 +0000

    build docker image & publish release

--- version.sh
+++ version.sh
@@ -5,21 +5,21 @@ ghost_version=$(grep -oP '(?<=FROM\sghost:)(.+)(?=-alpine)' Dockerfile | tail -1
 
 if [ -z "$ghost_version" ]; then
     echo "couldn't find a ghost version in Dockerfile"
-    return
+    exit 1
 fi
 
 echo "Found ghost:$ghost_version in Dockerfile"
 
 echo "Attempting to find revision..."
-revision=$(git tag | grep -oP "(?<=$ghost_version-)\d+")
+revision=$(git tag --sort=taggerdate | grep -oP "(?<=$ghost_version-)\d+" | tail -1)
 
 if [ -z "$revision" ]; then
     revision=0
 else
     echo "Found existing git tag for ghost version. Incrementing revision..."
-    revision=$(("$revision" + 1))
+    revision=$(($revision + 1))
 fi
 
 echo "Revision is $revision"
 
-echo "##vso[build.updatebuildnumber]$ghost_version-$revision"
+echo "##vso[task.setvariable variable=docker_tag]$ghost_version-$revision" 
